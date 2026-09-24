ad344f258bff675f8d46271a58a15c45edfe6cb3
monptm-rgb <mon.ptm@gmail.com>
2026-09-24 14:28:54 +0700

    Add files via upload

--- public/js/config.js
+++ public/js/config.js
@@ -1,4 +1,4 @@
 // ใส่ URL ของ Apps Script Web App ที่นี่ หลังจาก Deploy > New deployment > Web app
 // ตัวอย่าง: https://script.google.com/macros/s/xxxxxxxxxxxxxxxx/exec
-window.APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxsFtLp3efUtkhJearyKrugXyjxfZpyN6b-y-B4Gu6R-b3a0lgoJORJd64ZMQ6zeFXjJg/exec';
-window.SHOP_NAME = 'ร้าน มีดีกว่าที่คิด';
+window.APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycby_M1oMUXlcjxjohgKZV3en7O-i6O4gNLbraNt-4iwNr4Q54QUhOk7oBxSWDZ92nLSY/exec/exec';
+window.SHOP_NAME = 'ร้านโกดังขายส่ง';
